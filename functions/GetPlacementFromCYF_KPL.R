53a873c7a65fca76f4e7402204a5942e150748a6
nicolethegrape <ziz20@pitt.edu>
2017-03-26 18:14:58 -0400

    create TRUE/FALSE for crosssystem dataset

---
+++ functions/GetPlacementFromCYF_KPL.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyverse)
+#read in crosssystem data
+library(readxl)
+crosssystem<- read_excel("~/Desktop/17 SPRING/Capstone /DHS_CrossSystem.xlsx", sheet = "SystemInvolvement_EC2016")
+crosssystem<-rename(crosssystem, CrossID=CLIENT_ID, CYFK1=CYF_KPL_MIN_ACTIVE)
+
+datdumcross <- crosssystem %>%
+  select(CrossID, CYFK1) %>%
+  mutate(isplaced=!is.na(CYFK1)) %>%
+  group_by(CrossID) %>%
+  summarise(placedafter09=sum(isplaced) %>% as.logical)
