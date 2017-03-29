c88de15051111d8ee3923d7dcd9ca4a4cb371dcb
ShuningTong <sht72@pitt.edu>
2017-03-28 20:35:31 -0400

    refactor again

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-setwd("~/Desktop/Capstone") # change to where you put your data
+setwd("~/Desktop/Capstone/DHSTeam1") # change to where you put your data
 
 library(readxl)
 dat1 <- read_excel("DHS_Case_Clients_2016EntryCohort.xlsx", 
@@ -7,11 +7,11 @@ dat1 <- read_excel("DHS_Case_Clients_2016EntryCohort.xlsx",
 dat2 <- read_excel("DHS_CrossSystem.xlsx",
                    sheet = "SystemInvolvement_EC2016") # 8206 obs
 
-source("DHSTeam1/functions/shuning/GetPlacementFromACCEPT_REASON.R")
+source("functions/shuning/GetPlacementFromACCEPT_REASON.R")
 mergedData <- mergeData(dat1, dat2)
 placeData <- getPlaceData(mergedData)
 
-source("DHSTeam1/functions/shuning/GetPlacementFromCrossSystem.R")
+source("functions/shuning/GetPlacementFromCrossSystem.R")
 placeData2 <- getPlaceData2(mergedData)
 all(placeData$CASE_ID == placeData2$CASE_ID) # TRUE
 table(placeData$isPlacedFromAcceptReason) # from ACCEPT_REASON
@@ -22,13 +22,13 @@ placeDataNew <- placeData %>%
   mutate(isPlacedFromCrossSystem = placeData2$isPlacedFromCrossSystem) %>%
   mutate(isPlacedFromAny= isPlacedFromAcceptReason | isPlacedFromCrossSystem)
 
-source("DHSTeam1/functions/shuning/GetNumberOfServices.R")
+source("functions/shuning/GetNumberOfServices.R")
 serviceDataNew <- calAverNumServiceFamily(mergedData)
 
-source("DHSTeam1/functions/shuning/MergeServiceAndPlacement.R")
+source("functions/shuning/MergeServiceAndPlacement.R")
 finalData <- getFinalData(placeDataNew, serviceDataNew)
 
-source("DHSTeam1/functions/shuning/Boxplot.R")
+source("functions/shuning/Boxplot.R")
 library(ggplot2)
 generateBoxPlot(finalData)
 
