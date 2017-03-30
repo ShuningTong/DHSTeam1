488eeda4a7d57120b027fbb335e315f27df42d91
ShuningTong <sht72@pitt.edu>
2017-03-29 23:20:45 -0400

    add a second way of calculating number of services and generate boxplot

--- functions/shuning/Boxplot.R
+++ functions/shuning/Boxplot.R
@@ -1,24 +1,5 @@
-generateBoxPlot <- function(finalData){
-  ggplot(data=finalData, aes(x = as.factor(placement), y = averNumServiceFamily)) + 
-    geom_boxplot()
-}
-
-generateHousingBoxPlot <- function(finalData){
-  ggplot(data=finalData, aes(x = as.factor(placement), y = averNumHousingFamily)) + 
-    geom_boxplot()
-}
-
-generateBehaviorBoxPlot <- function(finalData){
-  ggplot(data=finalData, aes(x = as.factor(placement), y = averNumBehaviorFamily)) + 
-    geom_boxplot()
-}
-
-generateNutritionBoxPlot <- function(finalData){
-  ggplot(data=finalData, aes(x = as.factor(placement), y = averNumNutritionFamily)) + 
-    geom_boxplot()
-}
-
-generateMentalBoxPlot <- function(finalData){
-  ggplot(data=finalData, aes(x = as.factor(placement), y = averNumMentalFamily)) + 
-    geom_boxplot()
+generateBoxPlot <- function(finalData, yDataName, title){
+  ggplot(data=finalData, aes_string(x = "placement", y = yDataName)) + 
+    geom_boxplot() +
+    ggtitle(title)
 }
